396baf508bdf8b03ce46ff33e34b12eacc0620bc
Andrew Ye <yeh31@mcmaster.ca>
2022-10-21 01:29:04 -0400

    created vs project

--- dxwn-bot/dxwn.py
+++ dxwn-bot/dxwn.py
@@ -1,17 +1,14 @@
 import os
-import math
 import random
-import requests
-import asyncio
 import aiohttp
-import json
 import html
 import logging
 
 from dotenv import load_dotenv
 from discord.ext import commands
-from discord.ui import Button, View
-from discord import Intents, ButtonStyle, Embed, Colour
+from discord import Intents, Embed, Colour
+from TFView import TFView
+from MCView import MCView
 
 
 load_dotenv()
@@ -29,77 +26,6 @@ fmt = '[%(levelname)s] %(asctime)s - %(message)s'
 
 logging.basicConfig(filename = logfile, filemode = 'w', format = fmt, level = logging.DEBUG)
 
-class MCView(View):
-
-    q = None
-    author = None
-
-    def __init__(self, q, author):
-        super().__init__()
-
-        self.q = q
-        self.author = author
-
-        #randomize answers
-        answers = q['results'][0]['incorrect_answers']
-        answers.append(q['results'][0]['correct_answer'])
-        random.shuffle(answers)
-
-        #create button objects
-        for i, answer in enumerate(answers):                            
-            self.add_item(MCButton(label=answer, style=ButtonStyle.gray, row=math.floor(i/2)))  #buttons in two columns (2x2)
-
-class MCButton(Button):
-
-    async def callback(self, interaction):
-
-        #only command invoker can interact with MC questions
-        if interaction.user == self.view.author: 
-
-            if self.view.q['results'][0]['correct_answer'] == self.label:
-                self.style = ButtonStyle.green
-                await interaction.response.edit_message(content=f"{self.view.q['results'][0]['question']}\n\n**Correct!**", view=self.view)
-                self.view.stop()
-
-            else:
-                self.style = ButtonStyle.red
-                for item in self.view.children:
-                    if item.label == self.view.q['results'][0]['correct_answer']:
-                        item.style = ButtonStyle.green
-
-                await interaction.response.edit_message(content=f"{self.view.q['results'][0]['question']}\n\n**Incorrect...**", view=self.view)
-                self.view.stop()
-
-class TFView(View):
-
-    q = None
-    author = None
-
-    def __init__(self, q, author):
-        super().__init__()
-
-        self.q = q
-        self.author = author
-
-        #create button objects
-        self.add_item(TFButton(label='True', style=ButtonStyle.green))
-        self.add_item(TFButton(label='False', style=ButtonStyle.red))
-
-class TFButton(Button):
-
-    async def callback(self, interaction):
-
-        #only command invoker can interact with TF questions
-        if interaction.user == self.view.author:
-
-            if self.view.q['results'][0]['correct_answer'] == self.label:
-                await interaction.response.edit_message(
-                    content=f"{self.view.q['results'][0]['question']}\n\n**Correct!**", view=None)
-
-            else:
-                await interaction.response.edit_message(
-                    content=f"{self.view.q['results'][0]['question']}\n\n**Incorrect...the correct answer is {self.view.q['results'][0]['correct_answer']}**", view=None)
-     
 @bot.event #on ready
 async def on_ready():
     logging.info(f'{bot.user} is connected to:\n')
